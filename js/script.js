03189588113604c653a26be2964999baebe4a345
MariaPedroso-des <marhiapm@gmail.com>
2025-11-12 20:35:43 +0100

    intento resolver el problema JS con mucha ayuda de chatGPT

---
+++ js/script.js
@@ -0,0 +1,29 @@
+const personajes = document.getElementById('character-list')
+const nextPage = document.getElementById('next-page');
+const prevPage = document.getElementById('prev-page');
+let currentPage = 1;
+
+function getPersonajes (page) {
+  fetch(`https://rickandmortyapi.com/api/character/?page=${currentPage}`)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error('Mensaje de error');
+      }
+      return response.json();
+    })
+    .then((data) => {
+      const html = data.results.map (personaje => {
+        return `<li class ='personaje'>
+        <img src=${personaje.image}>
+        <h3>${personaje.name}</h3>
+        <p>${personaje.species}</p>
+        </li>`;
+      }).join('');
+    personajes.innerHTML = html  
+    })
+    .catch((error) => {
+      console.error(error);
+      personajes.innerHTML = 'Error';
+    })
+  }
+  
